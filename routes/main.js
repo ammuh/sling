339c3a4ad907a4bbf8bd30b13565ca448ba57cb1
Ammar Husain <ammo700@gmail.com>
2017-05-17 18:35:35 -0400

    Init Commit

---
+++ routes/main.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res) {
+    undefined.err;
+    res.send('Hello World');
+});
+module.exports = router;
